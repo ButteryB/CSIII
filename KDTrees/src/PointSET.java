979a220a54f8e11856fcc58284aef7a3afa28673
Buttery <103587116+ButteryB@users.noreply.github.com>
2023-02-01 16:15:33 +0000

    changes

--- KDTrees/src/PointSET.java
+++ KDTrees/src/PointSET.java
@@ -1,4 +1,3 @@
-
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
@@ -9,68 +8,88 @@ import java.util.TreeSet;
 
 public class PointSET {
 
-    private Set<Point2D> set;
+  private Set<Point2D> set;
 
-    public PointSET() {                     // construct an empty set of points 
+  public PointSET() {
+    set = new TreeSet<>();
+  } // construct an empty set of points
 
+  public boolean isEmpty() { // is the set empty?
+    return set.isEmpty();
+  }
+
+  public int size() { // number of points in the set
+    return set.size();
+  }
+
+  public void insert(Point2D p) {
+    checkIfNull(p);
+    set.add(p);
+  } // add the point to the set (if it is not already in the set)
+
+  public boolean contains(Point2D p) { // does the set contain point p?
+    checkIfNull(p);
+    return set.contains(p);
+  }
+
+  public void draw() { // draw all points to standard draw with p.draw()
+    StdDraw.setPenRadius(0.01);
+    StdDraw.setPenColor(StdDraw.BLACK);
+    for (Point2D p : set) {
+      p.draw();
     }
+  }
 
-    public boolean isEmpty() {              // is the set empty? 
-
-        return false;
+  public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle
+    Set<Point2D> range = new TreeSet<>();
+    for (Point2D p : set) {
+      if (rect.contains(p)) {
+        range.add(p);
+      }
     }
+    return range;
+  }
 
-    public int size() {                      // number of points in the set 
-        return 0;
+  public Point2D nearest(Point2D p) { // a nearest neighbor in the set to point p; null if the set is empty                       COOPILOT
+    checkIfNull(p);
+    if (set.isEmpty()) {
+      return null;
     }
-
-    public void insert(Point2D p) {             // add the point to the set (if it is not already in the set)
-
+    Point2D nearest = null;
+    double minDistance = Double.MAX_VALUE;
+    for (Point2D point : set) {
+      double distance = p.distanceSquaredTo(point);
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearest = point;
+      }
     }
+    return nearest;
+  }
 
-    public boolean contains(Point2D p) {           // does the set contain point p? 
-        return false;
+  private void checkIfNull(Object o) {
+    if (o == null) {
+      throw new java.lang.NullPointerException();
     }
+  }
 
-    public void draw() {                    // draw all points to standard draw with p.draw()
-        StdDraw.setPenRadius(0.01);
-        StdDraw.setPenColor(StdDraw.BLACK);
+  public static void main(String[] args) { // unit testing of the methods (optional)
+    String filename = "circle10.txt"; // args[0];
+    In in = new In(filename);
 
+    StdDraw.enableDoubleBuffering();
+
+    // initialize the data structures with N points from standard input
+    PointSET brute = new PointSET();
+    // KdTree kdtree = new KdTree();
+    while (!in.isEmpty()) {
+      double x = in.readDouble();
+      double y = in.readDouble();
+      Point2D p = new Point2D(x, y);
+      // kdtree.insert(p);
+      brute.insert(p);
     }
-
-    public Iterable<Point2D> range(RectHV rect) {           // all points that are inside the rectangle 
-
-        return null;
-    }
-
-    public Point2D nearest(Point2D p) {           // a nearest neighbor in the set to point p; null if the set is empty 
-
-        return null;
-    }
-
-    private void checkIfNull(Object o) {
-        if (o == null) {
-            throw new java.lang.NullPointerException();
-        }
-    }
-
-    public static void main(String[] args) {                // unit testing of the methods (optional) 
-        String filename = "circle10.txt";   // args[0];
-        In in = new In(filename);
-
-        StdDraw.enableDoubleBuffering();
-
-        // initialize the data structures with N points from standard input
-        PointSET brute = new PointSET();
-        // KdTree kdtree = new KdTree();
-        while (!in.isEmpty()) {
-            double x = in.readDouble();
-            double y = in.readDouble();
-            Point2D p = new Point2D(x, y);
-            // kdtree.insert(p);
-            brute.insert(p);
-        }
-        brute.draw();
-        StdDraw.show();
-    }
+    brute.draw();
+    StdDraw.show();
+  }
 }
